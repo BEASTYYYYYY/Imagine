aaa503d42014e793db6369b8959ac5a873914a68
Yash_kasera <125109632+BEASTYYYYYY@users.noreply.github.com>
2025-01-21 14:45:53 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse, type NextRequest } from "next/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-// Define public routes
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-
-export default clerkMiddleware(async (auth, req: NextRequest) => {
-    if (!isPublicRoute(req)) {
-        try {
-            // Protect private routes, ensuring only authenticated users can access
-            await auth.protect();
-        } catch (error) {
-            // Redirect to sign-in page if authentication fails
-            const signInUrl = new URL("/sign-in", req.url);
-            signInUrl.searchParams.set("redirect_url", req.url); // Optional: Pass the original URL for redirection after sign-in
-            return NextResponse.redirect(signInUrl);
-        }
-    }
-    // Allow public routes to proceed
-    return NextResponse.next();
-});
+export default clerkMiddleware()
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and static files unless explicitly matched
-        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-        // Always apply middleware for API and `trpc` routes
-        "/(api|trpc)(.*)",
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
     ],
-};
+}
