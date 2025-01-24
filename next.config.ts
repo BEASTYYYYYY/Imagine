85ac78412860c6c033693dc31f8315d1b9d7ac86
Yash_kasera <125109632+BEASTYYYYYY@users.noreply.github.com>
2025-01-24 13:49:42 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true
-  }
+    ignoreDuringBuilds: true, // Ignore ESLint errors during builds
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors during builds
+  },
+  experimental: {
+    middlewarePrefetch: "strict", // Prefetch middleware only when necessary
+  },
 };
 
 export default nextConfig;
